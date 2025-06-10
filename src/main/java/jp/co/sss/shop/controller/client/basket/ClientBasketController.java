4924db3c2db0a5c6c18992716be5413dd008f768
g-naito <g.naito.its.202504@gmail.com>
2025-06-10 17:29:23 +0900

    カート機能一覧表示・カート内削除実装途中

--- src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -1,5 +1,8 @@
 package jp.co.sss.shop.controller.client.basket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +15,72 @@ import jp.co.sss.shop.repository.ItemRepository;
 @Controller
 public class ClientBasketController {
 	
-	
+	final Integer DELETE_OREDER_NUM = 1;
 	/**
 	 * 商品情報
 	 */
 	@Autowired
 	ItemRepository itemRepository;
 	
+	
+	/**
+	 * カート一覧表示用
+	 * 商品カート追加実装時、スコープ初期化は消す 削除部分://
+	 * 
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping(path = "/client/basket/list")
-	public String addBasket(HttpSession session) {
-		BasketBean basketBean = new BasketBean(5,"ごぼう",5);
-		session.setAttribute("basketBeans", basketBean);
+	public String basketList(HttpSession session) {
+		List<BasketBean> basketBeans = new ArrayList<>();//
+		BasketBean basketBean1 = new BasketBean(5,"ごぼう",5);//
+		basketBean1.setOrderNum(2);// 
+		BasketBean basketBean2 = new BasketBean(6,"だんご",5);//
+		basketBean2.setOrderNum(2);// 
+		basketBeans.add(basketBean1);//
+		basketBeans.add(basketBean2);//
+		session.setAttribute("basketBeans", basketBeans);//
 		return "client/basket/list";
 	}
 	
+	
+	/**
+	 * カート内商品削除ボタン(注文数1減少、注文数0の場合商品削除)
+	 * 実装途中
+	 * 
+	 * @param session
+	 * @param basketBean
+	 * @return
+	 */
 	@RequestMapping(path = "/client/basket/delete", method = RequestMethod.POST)
-	public String itemList(BasketBean basketBean) {
+	public String basketDelete(HttpSession session,BasketBean basketBean) {
+		basketBean.setOrderNum((Integer)basketBean.getOrderNum()-DELETE_OREDER_NUM);
+//		BasketBean newBasketBean = new BasketBean();
 		
-		return  "redirect:/client/basket/list";
+		List<BasketBean> basketBeans = new ArrayList<>();
+		
+		basketBeans = (List<BasketBean>)session.getAttribute("basketBeans");
+		int num = 0;
+		for (BasketBean selectBasketBean:basketBeans) {
+			if(selectBasketBean.getId().equals(basketBean.getId())) {
+				basketBeans.set(num,basketBean); 
+			}
+			num += 1;
+		}
+
+		return  "client/basket/list";////カートへの追加メソッド出来次第リダイレクトに変更
+	}
+	
+	
+	/**
+	 * カートを
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(path = "/basket/allDelete")
+	public String basketAllDelete(HttpSession session) {
+		session.removeAttribute("basketBeans");
+		return  "client/basket/list"; //カートへの追加メソッド出来次第リダイレクトに変更
 	}
 	
 	

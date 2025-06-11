34dfdb634d2f536b1f9b5fad908d8ea3d42769ce
g-naito <g.naito.its.202504@gmail.com>
2025-06-11 15:49:11 +0900

    カート編集機能実装

--- src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -1,6 +1,7 @@
 package jp.co.sss.shop.controller.client.basket;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import jp.co.sss.shop.repository.ItemRepository;
 public class ClientBasketController {
 	
 	final Integer DELETE_OREDER_NUM = 1;
+	final Integer NO_OREDER = 0;
 	/**
 	 * 商品情報
 	 */
@@ -25,28 +27,57 @@ public class ClientBasketController {
 	
 	/**
 	 * カート一覧表示用
-	 * 商品カート追加実装時、スコープ初期化は消す 削除部分://
 	 * 
 	 * @param session
 	 * @return
 	 */
 	@RequestMapping(path = "/client/basket/list")
-	public String basketList(HttpSession session) {
-		List<BasketBean> basketBeans = new ArrayList<>();//
-		BasketBean basketBean1 = new BasketBean(5,"ごぼう",5);//
-		basketBean1.setOrderNum(2);// 
-		BasketBean basketBean2 = new BasketBean(6,"だんご",5);//
-		basketBean2.setOrderNum(2);// 
-		basketBeans.add(basketBean1);//
-		basketBeans.add(basketBean2);//
-		session.setAttribute("basketBeans", basketBeans);//
+	public String basketList() {
+		
 		return "client/basket/list";
 	}
+	
+	/**
+	 * カートへの商品追加用
+	 * 
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(path = "/client/basket/list/set")
+	public String basketSet(HttpSession session) {
+		List<BasketBean> basketList = new ArrayList<>();
+		BasketBean basketBean1 = new BasketBean(5,"ごぼう",5);
+		basketBean1.setOrderNum(2); 
+		BasketBean basketBean2 = new BasketBean(6,"だんご",5);//
+		basketBean2.setOrderNum(2);// 
+		
+		basketList.add(basketBean1);
+		basketList.add(basketBean2);
+		session.setAttribute("basketBeans", basketList);
+		
+		return "redirect:/client/basket/list";
+	}
+	
+	/**
+	 * カート追加実装途中
+	 * 
+	 * 
+	 */
+//	@RequestMapping(path = "/client/basket/list/set",method = RequestMethod.POST)
+//	public String basketAdd(HttpSession session) {
+//		List<BasketBean> basketBeans = new ArrayList<>();
+//		basketBeans = (List<BasketBean>)session.getAttribute("basketBeans");
+//		if(basketBeans.isEmpty()) {
+//			session.setAttribute("basketBeans", basketBeans);
+//			
+//		}
+//		return "redirect:client/basket/list";
+//	}
 	
 	
 	/**
 	 * カート内商品削除ボタン(注文数1減少、注文数0の場合商品削除)
-	 * 実装途中
+	 * 実装途中(リダイレクト：カート追加機能実装時に修正)
 	 * 
 	 * @param session
 	 * @param basketBean
@@ -54,32 +85,50 @@ public class ClientBasketController {
 	 */
 	@RequestMapping(path = "/client/basket/delete", method = RequestMethod.POST)
 	public String basketDelete(HttpSession session,BasketBean basketBean) {
-		basketBean.setOrderNum((Integer)basketBean.getOrderNum()-DELETE_OREDER_NUM);
-//		BasketBean newBasketBean = new BasketBean();
+		
 		
 		List<BasketBean> basketBeans = new ArrayList<>();
 		
+		//商品個数減少
 		basketBeans = (List<BasketBean>)session.getAttribute("basketBeans");
 		int num = 0;
 		for (BasketBean selectBasketBean:basketBeans) {
 			if(selectBasketBean.getId().equals(basketBean.getId())) {
-				basketBeans.set(num,basketBean); 
+				basketBeans.get(num).setOrderNum(basketBeans.get(num).getOrderNum() - DELETE_OREDER_NUM); 
 			}
+			
 			num += 1;
 		}
-
-		return  "client/basket/list";////カートへの追加メソッド出来次第リダイレクトに変更
+		
+		//商品の削除(注文数0のとき)
+		Iterator<BasketBean> iterator = basketBeans.iterator();
+		while (iterator.hasNext()) {
+			BasketBean item = iterator.next();
+	 
+	        if(item.getOrderNum().equals(NO_OREDER)) {
+	            iterator.remove();
+	        } 
+	    }
+		
+		//カートを空にする
+		if(basketBeans.isEmpty()) {
+			session.removeAttribute("basketBeans");
+		}
+		
+		return  "redirect: /client/basket/list";////カートへの追加メソッド出来次第リダイレクトに変更
+		
 	}
 	
 	
 	/**
-	 * カートを
+	 * カート内の商品を空にする
 	 * @param session
-	 * @return
+	 * @return　リダイレクト  : “/client/basket/list”
 	 */
-	@RequestMapping(path = "/basket/allDelete")
+	@RequestMapping(path = "/client/basket/allDelete",method = RequestMethod.POST)
 	public String basketAllDelete(HttpSession session) {
 		session.removeAttribute("basketBeans");
+
 		return  "client/basket/list"; //カートへの追加メソッド出来次第リダイレクトに変更
 	}
 	

5a209570f15480618988ada007c4e936379beead
so-ta <sota.arakaki@ftsnet.co.jp>
2025-06-11 11:19:21 +0900

    商品詳細画面へ遷移、いいね用のコントローラーファイル作成

--- src/main/java/jp/co/sss/shop/controller/client/item/ClientItemGoodController.java
+++ src/main/java/jp/co/sss/shop/controller/client/item/ClientItemGoodController.java
@@ -1,14 +1,66 @@
 package jp.co.sss.shop.controller.client.item;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.service.BeanTools;
+import jp.co.sss.shop.util.Constant;
 
 @Controller
 public class ClientItemGoodController {
+	/**
+	 * 商品情報
+	 */
+	@Autowired
+	ItemRepository itemRepository;
 	
-	@RequestMapping(path = "client/item/detail/{id}/")
-	public String showList() {
+	/**
+	 * Entity、Form、Bean間のデータコピーサービス
+	 */
+	@Autowired
+	BeanTools beanTools;
+	
+	/**
+	 * セッション
+	 */
+	@Autowired
+	HttpSession session;
+	
+	@RequestMapping(path = "/client/item/detail/{id}" , method = { RequestMethod.GET, RequestMethod.POST })
+	public String showItem(@PathVariable int id, Model model) {
+		
+		// 対象の商品情報を取得
+		Item item = itemRepository.findByIdAndDeleteFlag(id, Constant.NOT_DELETED);
+		
+		if (item == null) {
+			// 対象が無い場合、エラー
+			return "redirect:/syserror";
+		}
+
+		//Itemエンティティの各フィールドの値をItemBeanにコピー
+		ItemBean itemBean = beanTools.copyEntityToItemBean(item);
+
+		// 商品情報をViewへ渡す
+		model.addAttribute("item", itemBean);
+		//商品登録・変更・削除用のセッションスコープを初期化
+		session.removeAttribute("itemForm");
+		
 		return "client/item/detail";
 	}
+	
+	
+	@RequestMapping(path = "/client/item/list/1" , method = { RequestMethod.GET, RequestMethod.POST })
+	public String showList(Model model) {
+		System.out.println("お客さん！リストに遷移！！");
 
+		return "client/item/list";
+	}
 }

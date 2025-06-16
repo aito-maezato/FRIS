309b83619d9cc5eb393aad2dfeac47b9555f40dd
g-naito <g.naito.its.202504@gmail.com>
2025-06-16 17:59:57 +0900

    決済処理(処理3)～(処理5)

--- src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -1,5 +1,8 @@
 package jp.co.sss.shop.controller.client.order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,17 +15,31 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.bean.OrderItemBean;
 import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.Item;
 import jp.co.sss.shop.entity.User;
 import jp.co.sss.shop.form.OrderForm;
+import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.repository.OrderRepository;
 import jp.co.sss.shop.repository.UserRepository;
 import jp.co.sss.shop.util.Constant;
 
 @Controller
 public class ClientOrderRegistController {
 	
+	final Integer SOLDOUT = 0;
+	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	@Autowired
+	ItemRepository itemRepository;
 	
 	/**
 	 * 処理１
@@ -60,7 +77,7 @@ public class ClientOrderRegistController {
 	}
 	
 	/**
-	 * 動作テスト用(処理１テスト用)
+	 * 動作テスト用(処理１テスト用)testMethod
 	 * 
 	 * @param orderForm
 	 * @param session
@@ -124,6 +141,7 @@ public class ClientOrderRegistController {
 		OrderForm sessionOrderForm = (OrderForm)session.getAttribute("orderForm");
 		System.out.println("ここは処理３");
 		BeanUtils.copyProperties(orderForm, sessionOrderForm);
+		
 		 System.out.println(sessionOrderForm.getPostalCode());
 		 System.out.println(sessionOrderForm.getName());
 		 System.out.println(sessionOrderForm.getPhoneNumber());
@@ -154,6 +172,7 @@ public class ClientOrderRegistController {
 			System.out.println("からじゃないよ４");
 		}
 		System.out.println("\nここは処理４");
+		System.out.println(orderForm.getId());
 		System.out.println(orderForm.getPostalCode());
 		 System.out.println(orderForm.getName());
 		 System.out.println(orderForm.getPhoneNumber());
@@ -164,9 +183,112 @@ public class ClientOrderRegistController {
 	
 	//処理５
 	@RequestMapping(path = "/client/order/check",method = RequestMethod.POST)
-	public String SetCheckOrder(HttpSession session) {
+	public String SetCheckOrder(Integer payMethod,HttpSession session) {
+		OrderForm orderForm = (OrderForm)session.getAttribute("orderForm");
+		orderForm.setPayMethod(payMethod);
 		return "redirect:/client/order/check";
 	}
-	//処理7
+	
+	
+	
+	//処理6
+	//注文確認画面表示処理
+	@RequestMapping(path = "/client/order/check",method = RequestMethod.GET)
+		public String aa(HttpSession session, Model model) {
+			//注文情報の取得
+			OrderForm orderForm = (OrderForm)session.getAttribute("orderForm");
+			
+			//・セッションスコープから買い物かご情報を取得
+			List<BasketBean> basketList = new ArrayList<>();
+			basketList = (List<BasketBean>) session.getAttribute("basketBeans");
+			
+			//在庫チェック
+//			for(int i = 0; i < basketList.size(); i++) {
+//				//注文数が在庫数を超過した場合、注文数を在庫数と同数にする。商品名の名前記録
+//				if(basketList.get(i).getStock() < basketList.get(i).getOrderNum() && ! basketList.get(i).getStock().equals(SOLDOUT)) {
+//					itemNameListLessThan.add(basketList.get(i).getName());
+//					basketList.get(i).setOrderNum(basketList.get(i).getStock());
+//					
+//				}
+			
+				List<OrderItemBean> orderItemBeans = new ArrayList<>();
+				for(int i = 0; i < basketList.size(); i++) {
+					OrderItemBean orderItemBean = new OrderItemBean();
+					BeanUtils.copyProperties(basketList.get(i), orderItemBean);
+					Item item = itemRepository.getReferenceById(basketList.get(i).getId());
+					ItemBean itemBean = new ItemBean(); 
+					BeanUtils.copyProperties(item,itemBean);
+							//basketList.get(i).getId()
+				}
+//			Order order = orderRepository.getReferenceById(orderForm.getId());
+			
+			return "client/order/check";
+		}
+	
+	
+//	//処理6
+//	//注文確認画面表示処理
+//	@RequestMapping(path = "/client/order/check",method = RequestMethod.GET)
+//		public String aa(HttpSession session, Model model) {
+//			OrderForm orderForm = (OrderForm)session.getAttribute("orderForm");
+//			// 選択された注文情報に該当する情報を取得
+//			Order order = orderRepository.getReferenceById(orderForm.getId());
+//				
+//			//・セッションスコープから注文情報を取得
+//			session.getAttribute("");
+//			//・セッションスコープから買い物かご情報を取得
+//			List<BasketBean> basketList = new ArrayList<>();
+//			basketList = (List<BasketBean>) session.getAttribute("basketBeans");
+//			
+//			Iterator<BasketBean> iterator = basketList.iterator();
+//			
+//			//・注文商品の最新情報をDBから取得し、の在庫チェックをする
+//
+//			//・在庫不足、在庫切れ商品がある場合
+//			String aa = "a";
+//			if (aa == "aa") {
+//				//- 注文警告メッセージをリクエストスコープに保存
+//				
+//				while (iterator.hasNext()) {
+//					BasketBean item = iterator.next();
+//					
+//					//- 在庫数にあわせて、買い物かご情報を更新（注文数、在庫数)
+//					if (item.getId().equals(aa)) {
+//						
+//					}
+//					//- 在庫切れの商品は、買い物かごか情報ら削除
+//					if (item.getStock().equals(SOLDOUT)) {
+//						iterator.remove();
+//					}
+//				}
+//			}
+//			//・在庫状況を反映した買い物かご情報をセッションに保存
+//
+//			//・買い物かご情報から、商品ごとの金額小計を算出し、注文商品情報リストに保存
+//			while (iterator.hasNext()) {
+//				BasketBean item = iterator.next();
+//				item.getOrderNum();
+//			}
+//			//・注文商品情報リストから合計金額を算出する
+//			// 注文商品情報を取得
+//			List<OrderItemBean> orderItemBeanList = beanTools.generateOrderItemBeanList(order.getOrderItemsList());
+//			// 合計金額を算出
+//			int total = priceCalc.orderItemBeanPriceTotalUseSubtotal(orderItemBeanList);
+//
+//			//・合計金額をリクエストスコープに設定
+//			model.addAttribute("total", total);
+//
+//			//・注文商品情報リストをリクエストスコープに設定
+//			model.addAttribute("orderItemBeans", orderItemBeanList);
+//
+//			//・注文入力フォーム情報をリクエストスコープに設定
+//			OrderBean orderBean = beanTools.copyEntityToOrderBean(order);
+//			model.addAttribute("order", orderBean);
+//			//・注文確認画面表示
+//			return "forward:/";
+//		}
+//	}
+//	
+	
 	
 }
